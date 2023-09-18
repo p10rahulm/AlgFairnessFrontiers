bc2bc58efe2178c6a6ee32988dff8af6e7e5477a
Rahul Madhavan <rahulmadhavan@google.com>
2023-09-18 15:43:43 +0530

    content changes

--- syncAlgFairness.sh
+++ syncAlgFairness.sh
@@ -3,7 +3,7 @@ cd AlgFairnessFrontiers
 git pull origin main
 echo 'fetched latest files from git'
 cp index.html /var/www/events/algorithmic-fairness-2023/index.html
-cp ./* /var/www/events/algorithmic-fairness-2023/
-echo 'copied the files in base directory'
+# cp ./* /var/www/events/algorithmic-fairness-2023/
+echo 'index file copied from base directory to hosting directory'
 cp -R ./assets /var/www/events/algorithmic-fairness-2023/
 echo 'copied files in assets directory'
